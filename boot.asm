efb122dceed96e155270928e78e06135678a1a91
sunao <sunao@Matrix.local>
2018-02-09 17:44:09 +0800

    update

--- boot.asm
+++ boot.asm
@@ -2,7 +2,20 @@
 loop:
     jmp loop
 
-; Fill with 510 zeros minus the size of the previous code
+; When compiled , our program must fit into 512 bytes , 
+; with the last two bytes being the magic number , 
+; so here , tell our assembly compiler to pad out our 
+; program with enough zero bytes (db 0) to bring us to 
+; 510 th byte.
+
+; total 512 reserve 2 bytes ===> 510
+; $-$$ for statment size
+
+;DB - Define Byte. 8 bits
+;DW - Define Word. Generally 2 bytes on a typical x86 32-bit system
+;DD - Define double word. Generally 4 bytes on a typical x86 32-bit system
+
 times 510-($-$$) db 0
+
 ; Magic number
 dw 0xaa55 
