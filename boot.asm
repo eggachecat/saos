4a3a23548c394e748e4e9994327c22dc5b1c4b33
sunao <sunao_0626@hotmail.com>
2018-02-10 12:16:41 +0800

    some good stuff..

--- boot.asm
+++ boot.asm
@@ -1,41 +1,35 @@
-;
-; A simple boot sector that prints a message to the screen using a BIOS routine.
-;
-
-; something about assemble
-; registers: ax cs dx bv <- they can be accessed by cpu and us
-; EAX is the full 32-bit value
-
-; AX(BX CX DX) is the lower 16-bits registers
-; AL is the lower 8 bits of AX
-; AH is the bits 8 through 15 (zero-based) of AX
-
-;DB - Define Byte. 8 bits
-;DW - Define Word. Generally 2 bytes on a typical x86 32-bit system
-;DD - Define double word. Generally 4 bytes on a typical x86 32-bit system
+[org 0x7c00] ; no more pain for offsetting
 
 
+mov ah, 0x0e
 
-mov ah , 0x0e ; int(interrupt) which tells we want to use tty mode
-mov al , 'H'
-int 0x10 ; bascially if we interrupt with 0x10(this is general for video service) the cpu will verify the ah 
-		 ; and if it is 0x0e, the cpu will print the ascii in al; this routine is hard-coded by cpu
 
-mov al , 'e'
-int 0x10
-mov al , 'l'
-int 0x10
-mov al , 'l'
-int 0x10
-mov al , 'o'
-int 0x10
+; we want to play with stack!
+; two register: bp and sp for pointers at stack Base and stack Top(why not tp...)
+; bp stays still and sp is moving (the same as we know in data structure class)
+
+
+mov bp, 0x8000 ; as we says we set the base pointer at some free address
+mov sp, bp ; so now the top of stack is actually the base of the stack
+
+push 'A' ; push some data on the stack for later use
+push 'B' ; note, there are pushed on as 16-bit values
+push 'C' ; so the most significant byte will be added by our assembler as 0x00
+         ; and 'A' is something like 0x0(8 bits) + 'A'(8 bits)
+
+pop bx ;  Note , we can only pop 16-bits , so pop data to bx (cx dx are all fine!) bx has nothing to do with bp
+mov al, bl ; then copy bl ( i.e. 8-bit char ) to al
+int 0x10 ; print(al)
+
+the_secret:
+	db "X"
+
 jmp $ ; Jump to the current address ( i.e. forever ).
-
 
 
 ;
 ; Padding and magic BIOS number.
-;
+; Basiclly the remains code is for cpu to verify this is as bootloader
 
 ; When compiled , our program must fit into 512 bytes , 
 ; with the last two bytes being the magic number , 
