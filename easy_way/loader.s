f57d65bc17c1e1249aa03a32100707f98a45ac1a
sunao <sunao@hotmail.com>
2018-05-25 00:47:29 +0800

    where is my pokemon

---
+++ easy_way/loader.s
@@ -0,0 +1,33 @@
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+
+# multiboot stucture
+# contans something like the size of the RAM
+# in the ax register
+# copy magic number into bx register
+.section .multiboot
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
+.section .text
+.extern kernelMain
+.global loader
+
+loader:
+    mov $kernel_stack, %esp
+    push %eax
+    push %ebx
+    call kernelMain
+
+_stop:
+    cli
+    hlt
+    jmp _stop
+
+
+.section .bss
+.space 2*1024*1024; # 2 Mib
+kernel_stack:
